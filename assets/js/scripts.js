e74f4812b6fc2dbffd356fe91a27b27a19d7fdf1
raseltpi30 <142457868+raseltpi30@users.noreply.github.com>
2023-08-17 12:43:31 +0600

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+    $('.menu-btn').click(function(){
+        $('.menu').slideToggle(1000);
+    });
+
+    $(window).on('scroll',function(){
+        if($(this).scrollTop()){
+            $('.header-top').addClass('sticky')
+        }
+        else{
+            $('.header-top').removeClass('sticky')
+        }
+    });
+    $('.nav').onePageNav({
+        
+    });
+
+        $('#myCarousel').owlCarousel({
+            items:3,
+            autoplay:true,
+            autoplayTimeout:3000,
+            loop:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:2,
+                },
+                1000:{
+                    items:3,
+                }
+            }
+        });
+
+
+        $('.counter h2').counterUp({
+          delay: 10,
+          time: 2000,
+        });
+
+
+        var typed5 = new Typed('.change', {
+            strings: ['Love To Design', 'Love To Develop'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            cursorChar: "",
+            shuffle: false,
+            loop: true
+        });
+});
+AOS.init();
